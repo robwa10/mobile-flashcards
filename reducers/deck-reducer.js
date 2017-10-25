b1c2616fa537ccac91030c5bd56c980f83abb845
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-25 16:43:28 -0500

    Add deck reducer

---
+++ reducers/deck-reducer.js
@@ -0,0 +1,14 @@
+import {
+  GET_DECK,
+} from '../actions/action-constants';
+
+const deck = (state = {}, action) => {
+  switch (action.type) {
+    case GET_DECK:
+      return {
+        state[action.id]
+      }
+    default:
+      return state;
+  }
+}
