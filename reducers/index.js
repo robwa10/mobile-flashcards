472b86d0f57fddc79efb2dd4f1e77f660f1a9eea
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-25 10:09:57 -0500

    Add initial reducer

--- reducers/index.js
+++ reducers/index.js
@@ -2,3 +2,21 @@ import {
   ADD_DECK,
   ADD_CARD,
 } from '../actions/action-constants';
+import { combineReducers } from 'redux';
+
+const data = (state = {}, action) => {
+  switch (action.type) {
+    case ADD_DECK:
+      return {};
+    case ADD_CARD:
+      return {};
+    default:
+      return state;
+  }
+}
+
+const reducer = combineReducers({
+  data
+});
+
+export default reducer;
