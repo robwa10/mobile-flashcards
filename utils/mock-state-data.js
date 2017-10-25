43279b7e9c1442cbf7c26a2cf46fdf43fc05307e
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-25 16:53:20 -0500

    Move mock state to seperate file

---
+++ utils/mock-state-data.js
@@ -0,0 +1,53 @@
+export const mockState = {
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'Question 1?',
+        answer: 'Answer 1'
+      },
+      {
+        question: 'Question 2?',
+        answer: 'Answer 2'
+      }
+    ]
+  }
+}
+
+export const mockNewDeckState = {
+  JavaScript: {
+    title: 'JavaScript',
+    questions: []
+    }
+  }
+
+export const mockNewQuestionState = {
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'Question 1?',
+        answer: 'Answer 1'
+      }
+    ]
+    }
+}
+
+export const mockQuestion = {
+  question: 'Question 2?',
+  answer: 'Answer 2'
+}
+
+export const mockDeck = {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'Question 1?',
+        answer: 'Answer 1'
+      },
+      {
+        question: 'Question 2?',
+        answer: 'Answer 2'
+      }
+    ]
+  }
