4a478aa03194074683243726a286825e2e4a8619
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-24 21:19:34 -0500

    Add and test addCardToDeck

--- __tests__/action-creator-tests.js
+++ __tests__/action-creator-tests.js
@@ -1,5 +1,6 @@
 import {
   ADD_DECK,
+  ADD_CARD,
 } from '../actions/action-constants';
 import * as actions from '../actions';
 
@@ -7,9 +8,22 @@ describe('actions', () => {
   it('should create an action to add a deck', () => {
     const data = 'JavaScript';
     const expectedAction = {
-      type: 'ADD_DECK',
+      type: ADD_DECK,
       data,
     };
     expect(actions.addDeck(data)).toEqual(expectedAction)
-  })
+  });
+  it('should create an action to add card to a deck', () => {
+    const title = 'JavaScript';
+    const data = {
+      question: 'Which operator is used to concat strings?',
+      answer: '+',
+    };
+    const expectedAction = {
+      type: ADD_CARD,
+      title,
+      data,
+    };
+    expect(actions.addCardToDeck(title, data)).toEqual(expectedAction)
+  });
 })
--- actions/index.js
+++ actions/index.js
@@ -1,8 +1,15 @@
 import {
   ADD_DECK,
+  ADD_CARD,
 } from './action-constants';
 
 export const addDeck = data => ({
   type: ADD_DECK,
+  data,
+})
+
+export const addCardToDeck = (title, data) => ({
+  type: ADD_CARD,
+  title,
   data,
 })
