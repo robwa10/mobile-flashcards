025232d0488393fa0ca6ad9e16cfbe49f7d3457b
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-27 11:18:22 -0500

    Set initial class.

---
+++ containers/decks-list.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import connect from 'react-redux';
+
+class DecksList extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default connect()(DesksList);
