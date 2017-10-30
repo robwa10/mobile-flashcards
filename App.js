4275af38fcd133856d919e2cc67c61f38ef6650c
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-29 23:12:27 -0500

    Initial wiring of DecksList container

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createStore } from 'redux';
 import reducer from './reducers';
 import throttle from 'lodash/throttle';
 import { loadState, saveState } from './utils/localStorage';
+import DecksList from './containers/decks-list';
 
 const persistedState = loadState();
 const store = createStore(
@@ -23,9 +24,7 @@ export default class App extends React.Component {
     return (
       <Provider store={store}>
         <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
+          <DecksList />
         </View>
       </Provider>
     );
--- containers/decks-list.js
+++ containers/decks-list.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import connect from 'react-redux';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { getDeckList } from '../actions';
 
 class DecksList extends Component {
+  mapDeckTitles() {
+    let data = this.props.data
+    let myArray = [];
+    for(let key in data) {
+      myArray.push({key: key})
+    }
+    return myArray
+  }
+
   render() {
     return (
-
+      <FlatList
+        data={this.mapDeckTitles()}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+      />
     );
   }
 }
 
-export default connect()(DesksList);
+const mapStateToProps = ({ data }) => ({
+  data,
+})
+
+export default connect(mapStateToProps)(DecksList);
