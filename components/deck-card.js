03cbc27549f2d1ace065ff751fa10c96744694b7
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-30 09:20:23 -0500

    Refactor to TouchableHighlight

--- components/deck-card.js
+++ components/deck-card.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableHighlight } from 'react-native';
 
-export default function DeckCard({ title }) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{title}</Text>
-    </View>
+export default DeckCard = (
+    { title, onPress, touchableStyles, textStyles }) => {
+    return (
+      <TouchableHighlight
+        onPress={onPress}
+        style={touchableStyles}
+        >
+          <Text style={textStyles}>{title}</Text>
+      </TouchableHighlight>
   )
 };
-
-const styles = StyleSheet.create({
-  container: {
-    alignSelf: 'stretch',
-    marginHorizontal: 5,
-    marginVertical: 2,
-  },
-  text: {
-    textAlign: 'center',
-    fontSize: 24,
-    color: '#212121',
-    backgroundColor: '#FF8A65',
-    paddingVertical: 20,
-    marginVertical: 2,
-    borderRadius: 10,
-  }
-});
--- containers/decks-list.js
+++ containers/decks-list.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
 import { connect } from 'react-redux';
 import DeckCard from '../components/deck-card';
 
 class DecksList extends Component {
+
   mapDeckTitles() {
     let myArray = [];
     for(let key in this.props.data) {
@@ -12,11 +13,24 @@ class DecksList extends Component {
     return myArray
   }
 
+  renderDeckCard = (item) => (
+    <DeckCard
+      onPress={() => this.getCard(item.key)}
+      title={item.key}
+      touchableStyles={styles.touchableStyles}
+      textStyles={styles.textStyles}
+    />
+  )
+
+  getCard(title) {
+    console.log(title);
+  }
+
   render() {
     return (
       <FlatList
         data={this.mapDeckTitles()}
-        renderItem={({item}) => <DeckCard title={item.key} />}
+        renderItem={({item}) => this.renderDeckCard(item)}
       />
     );
   }
@@ -27,3 +41,18 @@ const mapStateToProps = ({ data }) => ({
 })
 
 export default connect(mapStateToProps)(DecksList);
+
+const styles = StyleSheet.create({
+  touchableStyles: {
+    backgroundColor: '#FF8A65',
+    marginHorizontal: 5,
+    marginVertical: 2,
+    borderRadius: 10,
+  },
+  textStyles: {
+    textAlign: 'center',
+    fontSize: 24,
+    color: '#212121',
+    paddingVertical: 20,
+  }
+});
