fb664d338e56de44fbcf9c677302194443766890
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-31 09:28:05 -0500

    Make more readable

--- components/deck-info-card.js
+++ components/deck-info-card.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 
 export default DeckInfoCard = (
-    { title, cards, onPress, parentStyles, textStyles }) => {
+    { title,
+      cards,
+      onPress,
+      containerStyles,
+      titleStyles,
+      textStyles }) => {
     return (
       <TouchableHighlight onPress={onPress}>
-        <View style={parentStyles}>
-          <Text style={textStyles}>{title}</Text>
+        <View style={containerStyles}>
+          <Text style={titleStyles}>{title}</Text>
           <Text style={textStyles}>{`${cards} cards`}</Text>
         </View>
       </TouchableHighlight>
