73a27dfe1810a0131d879da4bfd392e07551620b
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-10-31 09:28:37 -0500

    Add addDeck

--- containers/decks-list.js
+++ containers/decks-list.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { StyleSheet, FlatList } from 'react-native';
+import { StyleSheet, FlatList, TouchableHighlight, View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import DeckInfoCard from '../components/deck-info-card';
+import { addDeck, getDeck } from '../actions';
 
 class DecksList extends Component {
-
   mapDeckTitles() {
     let data = this.props.data;
     let myArray = [];
@@ -17,26 +17,34 @@ class DecksList extends Component {
     return myArray
   }
 
+  newCard = () => (this.props.dispatch(addDeck(this.state.text)));
+
+  getCard = (title) => (this.props.dispatch(getDeck(title)));
+
   renderDeckCard = (item) => (
     <DeckInfoCard
       onPress={() => this.getCard(item.key)}
       title={item.key}
       cards={item.cards}
-      parentStyles={styles.parentStyles}
+      containerStyles={styles.containerStyles}
+      titleStyles={styles.titleStyles}
       textStyles={styles.textStyles}
     />
   )
 
-  getCard(title) {
-    console.log(title);
-  }
-
   render() {
     return (
-      <FlatList
-        data={this.mapDeckTitles()}
-        renderItem={({item}) => this.renderDeckCard(item)}
-      />
+      <View>
+        <FlatList
+          data={this.mapDeckTitles()}
+          renderItem={({item}) => this.renderDeckCard(item)}
+        />
+        <TouchableHighlight onPress={() => this.newCard()}>
+          <View style={styles.containerStyles}>
+            <Text style={styles.titleStyles}>Add a new deck</Text>
+          </View>
+        </TouchableHighlight>
+      </View>
     );
   }
 }
@@ -48,15 +56,20 @@ const mapStateToProps = ({ data }) => ({
 export default connect(mapStateToProps)(DecksList);
 
 const styles = StyleSheet.create({
-  parentStyles: {
+  containerStyles: {
     backgroundColor: '#FF8A65',
-    paddingVertical: 20,
+    paddingVertical: 30,
     marginHorizontal: 5,
     marginVertical: 2,
   },
-  textStyles: {
+  titleStyles: {
     textAlign: 'center',
     fontSize: 24,
+    color: '#212121',
+  },
+  textStyles: {
+    textAlign: 'center',
+    fontSize: 18,
     color: '#212121',
   }
 });
