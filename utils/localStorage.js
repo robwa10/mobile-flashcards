1f74c93fb5295c78bf385854940bd43476d1961a
Robert Hubbard <rob.c.hubbard@gmail.com>
2017-11-03 13:37:18 -0500

    refactor

--- utils/localStorage.js
+++ utils/localStorage.js
@@ -1,22 +1,26 @@
 import { AsyncStorage } from 'react-native';
 
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    AsyncStorage.setItem('state', serializedState);
+  } catch (err) {
+    console.log("There was a write error.");
+  }
+};
+
 export const loadState = () => {
   try {
-    const serializedState = AsyncStorage.getItem('state:key');
-    if (serializedState === null) {
-      return undefined;
+    const serializedState = AsyncStorage.getItem('state');
+    if (serializedState !== null) {
+      console.log('Returned State');
+      console.log(JSON.parse(serializedState));
+      return JSON.parse(serializedState);
     }
-    return JSON.parse(serializedState);
+    console.log('Returned undefined');
+    return undefined;
   } catch (err) {
     return undefined;
   }
 };
 
-export const saveState = (state) => {
-  try {
-    const serializedState = JSON.stringify(state);
-    AsyncStorage.setItem('state:key', serializedState);
-  } catch (err) {
-    console.log("There was a write error.");
-  }
-};
